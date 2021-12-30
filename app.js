3f420e653876b274e36b9fc45efe6cf18a797c2f
Roonny Ruiz <roonny7@gmail.com>
2021-12-29 21:44:14 -0500

    fin del 29 de dic

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+const { mostrarMenu } = require('./helpers/mensajes');
+const { inquirerMenu, pausa} = require ('./helpers/inquirer');
 require('colors');
 
+console.clear();
+
+
 const main = async() => {
-    console.clear();
-    console.log("hola mundo")
+    
+    console.log("hola mundo");
+    mostrarMenu();
+
+    let opt = '';
+    
+    do {
+    
+      opt = await inquirerMenu();
+      console.log({opt});
+
+      await pausa();
+          
+    }while ( opt !== '0');
+
+
+   //pausa(); 
 
 
 }
